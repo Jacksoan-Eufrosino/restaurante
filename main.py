79d77d0ae8292949bd3330b57c81a141628edadd
Anthony Araújo <129444897+anthonyllz@users.noreply.github.com>
2023-12-10 23:41:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,35 @@
-from ler_csv import *
+from indices import *
 
-class No:
-    def __init__(self, carga=None, proximo=None):
-        self.carga = carga
-        self.proximo = proximo
+class Service:
+    def __init__(self, restaurant_reposoitory):
+        self.restaurant_repository = restaurant_reposoitory
 
-    def __repr__(self):
-        return '%s -> %s' % (self.carga, self.proximo)
-
-class ListaEncadeada:
-    def __init__(self, head:'No'=None, tail:'No'=None):
-        self.head = head
-        self.tail = tail
-
-    def inserir(self, carga):
-        novo = No(carga)
-        if self.head is None:
-            self.head = self.tail = novo
-        else:
-            self.tail.proximo = novo
-            self.tail = novo
-
-    def buscar(self, chave:'str'):
-        atual = self.head
+    def busca(self, query):
         results = []
-        while atual.proximo is not None:
-            if chave.title() == atual.carga['name'][:len(chave)]:
-                results.append(atual.carga)
-            atual = atual.proximo
-        return results
 
-    def __str__(self):
-        return str(self.head)
+        if query.name:
+            nomes_encontrados = restaurante.buscar(query.name)
+            results.append(nomes_encontrados)
+            if nomes_encontrados is None:
+                results.append(RestaurantRepository.find_all())
+
+        if query.customer_rating:
+            results.append(rating.maior_igual(int(query.customer_rating)))
+
+        if query.distance:
+            results.append(distancia.menor_igual(int(query.distance)))
+
+        if query.price:
+            results.append(valor.menor_igual(int(query.price)))
+
+        if not query.distance and not query.customer_rating and not query.price and not query.name and not query.cuisine_id:
+            results.append(RestaurantRepository().find_all())
+
+        return leitura_csv.find_intersection(results)
 
 if __name__ == '__main__':
-    lista = ListaEncadeada()
-    dados = read_csv('restaurants.csv')
-    for item in dados:
-        lista.inserir(item)
-
-    for i in lista.buscar('Del'):
-        if int(i['distance']) < 2:
-            print(i)
+    service1 = Service(RestaurantRepository)
+    restaurante1 = Restaurant(None, 5, 7, 10, 0) #Nome - Classificação - Distância - Preço - Cozinha
+                                                    #Falta cuisine
+    for r in service1.busca(restaurante1):
+        print(r)
